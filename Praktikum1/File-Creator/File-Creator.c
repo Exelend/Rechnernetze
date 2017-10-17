90364da5319a4800b7b7fe423c5b5796a5971f52
Martin <Martin.Witte@haw-hamburg.de>
2017-10-17 22:46:40 +0200

    TestdateiErstellung fertig\nFile-Creator Perameterübernahme angefangen.

--- Praktikum1/File-Creator/File-Creator.c
+++ Praktikum1/File-Creator/File-Creator.c
@@ -15,6 +15,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#include <sys/types.h>// für Linux
+#include <sys/stat.h> // für Linux
+
+#include <io.h>       // für Windows
+#include <sys\stat.h> // für Windows
+
+#include <fcntl.h>
+
 
 #include "File-Creator.h"
 /*
@@ -27,6 +35,7 @@ static FILE* create(char* name){
     if(name != NULL){
         pFile = fopen(name, "w");
         fclose(pFile);
+        pFile = NULL;
     }
     return pFile;
 }
@@ -34,20 +43,36 @@ static FILE* create(char* name){
 
 int main(int argc, char* argv[]) {
     
+    char * pFilename = NULL;
+    int MAXanzahlThreads = 0;
+    int curAnzahlThreads = 0;
+    
     if(argc > 1){
-        
+        int temp = 0;
+        for(int i = 0; i < argc; i++) {
+            int j = 0;
+            if(strchr("-C", argv[i])){
+                MAXanzahlThreads = atoi(argv[i+1]);
+            } else if(temp = open(argv[i], O_RDONLY) <= 0){ // Lesender Zugriff auf Datei möglich?    
+                // Dateiname
+                pFilename = argv[i];
+                close(temp);
+            }
+            j++;
+	}
+        if(pFilename == NULL){
+            printf("Datei nicht lesbar");
+            return EXIT_FAILURE;
+        }
+    } else {
+        printf("Bitte Textdatei mit Namen uebergeben!");
     }
     // TODO args trennen und prüfen.
     
-    int MAXanzahlThreads = 0;
-    int curAnzahlThreads = 0;
-    pthread_t threadArray[MAXanzahlThreads] = {};
+    pFile = fopen();
+    FILE * pFile = NULL;
     
-    FILE * pNamen = fopen();
-    do{
-        // TODO Threads starten und verwalten
-    } while(//TODO);
-        
+    
     return (EXIT_SUCCESS);
 }
 
