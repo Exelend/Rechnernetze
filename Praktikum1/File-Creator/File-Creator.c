563c10ec706dcb6ece2dec3d61fd31fd679851aa
Martin <Martin.Witte@haw-hamburg.de>
2017-10-18 19:58:27 +0200

    File-Creator:\nEinlesen von args fretig.

--- Praktikum1/File-Creator/File-Creator.c
+++ Praktikum1/File-Creator/File-Creator.c
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   File-Creator.c
  * Author: MaWi
  *
  * Created on 17. Oktober 2017, 10:52
+ * 
+ * 
+ * Compile with -pthread
  */
 
 #include <stdio.h>
@@ -30,49 +27,63 @@
  */
 
 // Thread Create
-static FILE* create(char* name){
-    FILE * pFile = NULL;
-    if(name != NULL){
-        pFile = fopen(name, "w");
-        fclose(pFile);
-        pFile = NULL;
-    }
-    return pFile;
+
+void hilfetextAusgeben(){
+    printf("usage: File-Creator [-c <MaxAnzahlThreads>] <NAMENDATEI>");
 }
 
+static FILE* create(char* name) {
+    FILE * pFile = NULL;
+    if (name != NULL) {
+        pFile = fopen(name, "w");
+        if (pFile != NULL) {
+            fclose(pFile);
+            pFile = NULL;
+        }
+    }
+    exit(3);
+}
 
 int main(int argc, char* argv[]) {
-    
+
     char * pFilename = NULL;
-    int MAXanzahlThreads = 0;
+    int MAXanzahlThreads = 3;
     int curAnzahlThreads = 0;
-    
-    if(argc > 1){
-        int temp = 0;
-        for(int i = 0; i < argc; i++) {
-            int j = 0;
-            if(strchr("-C", argv[i])){
-                MAXanzahlThreads = atoi(argv[i+1]);
-            } else if(temp = open(argv[i], O_RDONLY) <= 0){ // Lesender Zugriff auf Datei möglich?    
-                // Dateiname
-                pFilename = argv[i];
-                close(temp);
-            }
-            j++;
-	}
-        if(pFilename == NULL){
-            printf("Datei nicht lesbar");
-            return EXIT_FAILURE;
+
+    int c;
+
+    while ((c = getopt(argc, argv, "C:") != -1)) {
+        switch (c) {
+            case 'C':
+                MAXanzahlThreads = atoi(optarg);
+                break;
+            case '?':
+                hilfetextAusgeben();
+                return EXIT_FAILURE;
+                break;
         }
-    } else {
-        printf("Bitte Textdatei mit Namen uebergeben!");
     }
-    // TODO args trennen und prüfen.
     
-    pFile = fopen();
+    if(MAXanzahlThreads <= 0){
+        printf("!!! Für -C <Ganzzahl>0> !!!");
+        return EXIT_FAILURE;
+    }
+
+    pFilename = argv[argc-1];
+    
+    FILE pFile = NULL;
+    if(pFile = fopen() == NULL){
+        printf("Datei nicht lesbar oder nicht übergeben.");
+        return EXIT_FAILURE;
+    }
+
+    pthread_t [] = MAXanzahlThreads;
+    
+    
+    close(pFile);
     FILE * pFile = NULL;
-    
-    
+
+
     return (EXIT_SUCCESS);
 }
 
